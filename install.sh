0bd9b69e6827473d1442483d1ea86a8e2e79d936
Claudio Walser <claudio@claudio-pc.development.claudio.dev>
2015-05-27 22:50:03 +0200

    added patch and re-organized a bit

--- install.sh
+++ install.sh
@@ -17,8 +17,33 @@ runuser -l  $SUDO_USER -c "touch /home/${SUDO_USER}/Vorlagen/Neue\ Datei.txt"
 dnf install --nogpgcheck https://download.jitsi.org/jitsi/rpm/jitsi-2.8-5426.x86_64.rpm
 
 # install some main packages now
-dnf install gcc nano fuse-sshfs tomboy keepass deja-dup kernel-devel kernel-headers screen vlc gnome-tweak-tool libreoffice conky screenfetch alacarte pidgin amule gimp \
-            gstreamer-plugins-ugly gstreamer-plugin-crystalhd gstreamer1-libav gstreamer1-plugins-bad-freeworld gstreamer1-vaapi gstreamer1-plugins-good gstreamer1-plugins-bad-free 
+dnf install patch \
+            gcc \
+            nano \
+            fuse-sshfs \
+            tomboy \
+            keepass \
+            deja-dup \
+            kernel-devel \
+            kernel-headers \
+            screen \
+            vlc \
+            gnome-tweak-tool \
+            libreoffice \
+            conky \
+            screenfetch \
+            alacarte \
+            pidgin \
+            amule \
+            gimp \
+            dconf-editor \
+            gstreamer-plugins-ugly \
+            gstreamer-plugin-crystalhd \
+            gstreamer1-libav \
+            gstreamer1-plugins-bad-freeworld \
+            gstreamer1-vaapi \
+            gstreamer1-plugins-good \
+            gstreamer1-plugins-bad-free 
 
 # install paper-gtk-theme and paper-icon-theme cause they are hot
 runuser -l  $SUDO_USER -c "mkdir -p /home/${SUDO_USER}/Development/paper-gtk-theme"
